4c691c2cb9113c1234dda682f9938f33e157d2d8
Rohde Fischer <rohdef@rohdef.dk>
2025-01-06 10:28:36 +0100

    Adding default OCI labels

--- assets/README.md
+++ assets/README.md
@@ -1,3 +1,37 @@
 # Action for building, containers
 
 See [source](https://github.com/rohdef/action-build-container-source) for source code
+
+## Labels
+
+OCI recommends a set of [default labels](https://github.com/opencontainers/image-spec/blob/main/annotations.md)
+where this action inserts reasonable default values when possible.
+Please make sure to set the rest/customize what is needed for your needs.
+
+```yaml
+with:
+  labels: |
+    org.opencontainers.image.authors=Fiktivus Maximus (maximus@dummy-email)
+    org.opencontainers.image.url=https://project-domain.com
+    org.opencontainers.image.documentation=https://project-domain.com/documentation
+    org.opencontainers.image.vendor=Super Organization Inc
+    org.opencontainers.image.ref.name=3.1-super-project
+    org.opencontainers.image.title=The Super Prject
+    org.opencontainers.image.description=This project does super things
+```
+
+| label                                  | description                                  | default value                         | should be set |
+|----------------------------------------|----------------------------------------------|---------------------------------------|---------------|
+| org.opencontainers.image.created       | Build date in full ISO8601                   | Current                               | No            |
+| org.opencontainers.image.authors       | People or organization responsible for image | $GITHUB_ACTOR                         | Yes           |
+| org.opencontainers.image.url           | URL for information about image              | $GITHUB_SERVER_URL/$GITHUB_REPOSITORY | Yes           |
+| org.opencontainers.image.documentation | Image documentation                          | $GITHUB_SERVER_URL/$GITHUB_REPOSITORY | Yes           |
+| org.opencontainers.image.source        | Image source                                 | $GITHUB_SERVER_URL/$GITHUB_REPOSITORY | No            |
+| org.opencontainers.image.version       | Image version                                |                                       |               |
+| org.opencontainers.image.revision      | Source control revision identifier           | $GITHUB_SHA                           | No            |
+| org.opencontainers.image.vendor        | Distributing entity                          | $GITHUB_ACTOR                         | Yes           |
+| org.opencontainers.image.licenses      | License                                      |                                       | Yes           |
+| org.opencontainers.image.ref.name      | Name of the reference for a target           |                                       | Yes           |
+| org.opencontainers.image.title         | Human-readable title                         |                                       | Yes           |
+| org.opencontainers.image.description   | Human-readable description                   |                                       | Yes           |
+
--- src/jsMain/kotlin/dk/rohdef/actions/Main.kt
+++ src/jsMain/kotlin/dk/rohdef/actions/Main.kt
@@ -4,8 +4,10 @@ import com.docker.actions_toolkit.lib.docker.Docker
 import com.docker.actions_toolkit.lib.github.GitHub
 import dk.rohdef.actions.dk.rohdef.actions.github.Core
 import kotlinx.coroutines.await
+import node.process.Process
 import node.process.process
-    import kotlin.uuid.ExperimentalUuidApi
+import kotlin.js.Date
+import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
 suspend fun Core.actionInfo() {
@@ -27,7 +29,28 @@ suspend fun Core.actionInfo() {
     }
 }
 
-@OptIn(ExperimentalUuidApi::class, ExperimentalJsCollectionsApi::class)
+class ActionEnvironment(
+    val process: Process,
+) {
+    fun Process.getEnvironmentValue(key: String): String =
+        process.env[key] ?: throw IllegalArgumentException("$key environment variable is not set")
+
+    val ACTOR = "GITHUB_ACTOR"
+    val COMMIT_SHA = "GITHUB_SHA"
+    val REPOSITORY = "GITHUB_REPOSITORY"
+    val RUN_ID = "GITHUB_RUN_ID"
+    val SERVER_URL = "GITHUB_SERVER_URL"
+
+    val actor = process.getEnvironmentValue(ACTOR)
+    val commitSha = process.getEnvironmentValue(COMMIT_SHA)
+    val repository = process.getEnvironmentValue(REPOSITORY)
+    val runId = process.getEnvironmentValue(RUN_ID)
+    val serverUrl = process.getEnvironmentValue(SERVER_URL)
+
+    val projectUrl = "$serverUrl/$repository"
+}
+
+@OptIn(ExperimentalUuidApi::class)
 suspend fun main() {
     Core().run(
         {
@@ -37,12 +60,26 @@ suspend fun main() {
             setOutput("imageid", "action: ${imageName}")
 //            listOf("--tag", imageName.toString())
 
-            // TODO rohdef - find a way to wrap the map
-            val environment = process.env
-            val runId = environment["GITHUB_RUN_ID"] ?: throw IllegalArgumentException("GITHUB_RUN_ID env variable is not set")
-            val labels = mapOf("runnumber" to runId) + inputs.labels.value
+            val actionEnvironment = ActionEnvironment(process)
+
+            val date = Date().toISOString()
+            val defaultLabels = mapOf(
+                "runnumber" to actionEnvironment.runId,
+                "org.opencontainers.image.created" to date,
+                "org.opencontainers.image.authors" to actionEnvironment.actor,
+                "org.opencontainers.image.url" to actionEnvironment.projectUrl,
+                "org.opencontainers.image.documentation" to actionEnvironment.projectUrl,
+                "org.opencontainers.image.source" to actionEnvironment.projectUrl,
+                "org.opencontainers.image.version" to "not specified",
+                "org.opencontainers.image.revision" to actionEnvironment.commitSha,
+                "org.opencontainers.image.vendor" to actionEnvironment.actor,
+                "org.opencontainers.image.licenses" to "not specified",
+                "org.opencontainers.image.ref.name" to "not specified",
+                "org.opencontainers.image.title" to "not specified",
+                "org.opencontainers.image.description" to "not specified",
+            )
+            val labels = defaultLabels + inputs.labels.value
             info("lbls: $labels")
-            info("Running $runId")
 
 
             // getArgs
